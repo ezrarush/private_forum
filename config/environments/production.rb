fa4af7f8b29420bde50e843f4ac3783c2e27af3f
Ezra Rush <rushwest@gmail.com>
2015-05-01 14:56:40 -0500

    debugging heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -74,21 +74,21 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port: 587,
-    domain: Rails.application.secrets.domain_name,
+    #domain: Rails.application.secrets.domain_name,
     authentication: "plain",
     enable_starttls_auto: true,
     user_name: Rails.application.secrets.email_provider_username,
-    password: Rails.application.secrets.email_provider_password,
-    :openssl_verify_mode => 'none' # heroku error
+    password: Rails.application.secrets.email_provider_password
   }
   # ActionMailer Config
-  config.action_mailer.default_url_options = { :host => Rails.application.secrets.domain_name }
+  config.action_mailer.default_url_options = { :host => Rails.application.secrets.domain_name, :protocol => 'http' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default :charset => "utf-8"
 
-  #for heroku error
-  #Rails.application.routes.default_url_options[:host] = Rails.application.secrets.domain_name
+  #for heroku
+  Rails.application.routes.default_url_options[:host] = Rails.application.secrets.domain_name
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new

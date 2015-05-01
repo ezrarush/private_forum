f8eb29f858b3740091a4574260fe11f9bc1e86a1
Ezra Rush <rushwest@gmail.com>
2015-05-01 14:35:26 -0500

    debugging heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -74,11 +74,12 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port: 587,
-    domain: "gmail.com", #Rails.application.secrets.domain_name,
+    domain: Rails.application.secrets.domain_name,
     authentication: "plain",
     enable_starttls_auto: true,
     user_name: Rails.application.secrets.email_provider_username,
-    password: Rails.application.secrets.email_provider_password
+    password: Rails.application.secrets.email_provider_password,
+    :openssl_verify_mode => 'none' # heroku error
   }
   # ActionMailer Config
   config.action_mailer.default_url_options = { :host => Rails.application.secrets.domain_name }

53ee7a1d73dbc044b1b869c346f68d6bd084eb8b
Ezra Rush <rushwest@gmail.com>
2015-05-02 00:33:31 -0500

    view clean up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,11 +2,9 @@ class UsersController < ApplicationController
   load_and_authorize_resource
   
   def index
-  #  @users = User.all
   end
 
   def show
-   # @user = User.find(params[:id])
   end
 
 end
